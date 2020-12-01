552ec04cff758a9d834a024ad7bad6344c79a3e4
Marc Sinner <guitarmax10@gmail.com>
2020-12-01 21:32:13 +0100

    finished bunny

--- exercise_3_src/ProcrustesAligner.h
+++ exercise_3_src/ProcrustesAligner.h
@@ -14,7 +14,7 @@ public:
 		auto targetMean = computeMean(targetPoints);
 		
 		Matrix3f rotation = estimateRotation(sourcePoints, sourceMean, targetPoints, targetMean);
-		Vector3f translation = computeTranslation(sourceMean, targetMean);
+		Vector3f translation = computeTranslation(sourceMean, targetMean, rotation);
 
 		// To apply the pose to point x on shape X in the case of Procrustes, we execute:
 		// 1. Translation of a point to the shape Y: x' = x + t
@@ -24,6 +24,19 @@ public:
 		// TODO: Compute the transformation matrix by using the computed rotation and translation.
 		// You can access parts of the matrix with .block(start_row, start_col, num_rows, num_cols) = elements
 		Matrix4f estimatedPose = Matrix4f::Identity();
+		
+		for (unsigned int i = 0; i<rotation.rows(); i++){ 
+			for (unsigned int j = 0; j<rotation.cols(); j++){ 
+				estimatedPose(i,j) = rotation(i,j);
+			}
+		}
+
+		for (unsigned int j = 0; j<translation.size(); j++){ 
+				estimatedPose(j,3) = translation(j);
+		}
+
+
+
 
 		return estimatedPose;
 	}
@@ -34,9 +47,7 @@ private:
 		Vector3f mean = Vector3f::Zero();
 
 		for (unsigned int i = 0; i<mean.size(); i++){
-			mean(0) = mean(0) + points[i].x();
-			mean(1) = mean(1) + points[i].y();
-			mean(2) = mean(2) + points[i].z();
+			mean = mean+points[i];
 		}
 		mean(0) = mean(0)/points.size();
 		mean(1) = mean(1)/points.size();
@@ -49,38 +60,46 @@ private:
 		// TODO: Estimate the rotation from source to target points, following the Procrustes algorithm.
 		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
 		// Important: The covariance matrices should contain mean-centered source/target points.
-		Vector3f translation = computeTranslation(sourceMean, targetMean);
+		//Dynamic instead of "4" did not work - what did i do wrong? MatrixXf also didnt work
+		Eigen::Matrix<float,4,3> targetPointsCentered;
+		Eigen::Matrix<float,4,3> sourcePointsCentered;
 
-		std::vector<Vector3f> targetPointsCentered;
-		std::vector<Vector3f> sourcePointsCentered;
 
 
 		for (unsigned int i = 0; i<targetPoints.size(); i++){ //sizes are the same targetPoints.size == sourcePoints.size
 			//targetCovariance(0) = targetPoints[i] + sourceMean; 
-			targetPointsCentered.push_back((targetPoints[i] + sourceMean));
-			sourcePointsCentered.push_back((sourcePoints[i] + sourceMean));
+			targetPointsCentered.row(i) = ((targetPoints[i] + targetMean));
+			sourcePointsCentered.row(i)=((sourcePoints[i] + sourceMean));
 		}
+		
 
+		Eigen::Matrix3f covarianceMatrix = Matrix3f::Identity();;
 
-		//TODO: fill matrix (from target/sourcePointsCentered)
-		Eigen::Matrix3f targetCovariance;
-		Eigen::Matrix3f sourceCovariance;
-
-
+		covarianceMatrix = targetPointsCentered.transpose()*sourcePointsCentered;
 
 		Matrix3f rotation = Matrix3f::Identity();
-		JacobiSVD<Matrix3f> jacobi(targetCovariance.transpose() * sourceCovariance);
+		JacobiSVD<MatrixXf> jacobi(covarianceMatrix, ComputeThinU | ComputeThinV);
+
 		rotation = jacobi.matrixU() * jacobi.matrixV().transpose(); 
 
-		
+		int a = (int) rotation.determinant();
+		Matrix3f mirror;
+		mirror << 1,0,0,0,1,0,0,0,-1;
+
+		if (a<0){
+			rotation = jacobi.matrixU() *mirror* jacobi.matrixV().transpose();
+		}
+
 
 		return rotation;
 	}
 
-	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean) {
+	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean, Matrix3f rotation) {
 		// TODO: Compute the translation vector from source to target points.
 		Vector3f translation = Vector3f::Zero();
-		translation = targetMean - sourceMean;
+		// translation = targetMean - sourceMean;
+
+		translation = -rotation*sourceMean+ targetMean;
 
 		return translation;
 	}
